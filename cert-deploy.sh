d830ea089d18f56b5e7aeff2ded25bf4db76b83f
pveronneau <patrick@decepticons.ca>
2017-04-15 18:59:39 -0600

    Initial commit

---
+++ cert-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Deploy certificates from acme.sh to services
+
+####
+# Variables
+####
+HOSTNAME=`hostname`
+CERTLOCATION="/etc/$HOSTNAME.cert"
+
+####
+# Create Central File
+cat /root/.acme.sh/$HOSTNAME/$HOSTNAME.key > $CERTLOCATION && cat /root/.acme.sh/$HOSTNAME/$HOSTNAME.cer >> $CERTLOCATION
+chmod 600 $CERTLOCATION
+####
+
+####
+# Deploy to servies
+####
+# Copy host cert over default cockpit cert and restart
+cp $CERTLOCATION /etc/cockpit/ws-certs.d/0-self-signed.cert && systemctl restart cockpit
+# Copy host cert over default 3dm2 cert and restart
+cp $CERTLOCATION /etc/3dm2/3dm2.pem && systemctl restart tdm2
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HOSTNAME=`hostname`
+
+curl https://get.acme.sh | sh
+acme.sh --issue --dns -d $HOSTNAME
+echo "Add the TXT entry above before proceding"
+read -n1 -r -p "Press space to continue... or any other key to abort" key
+
+if [ "$key" = '' ]; then
+    echo "Renewing Certificate"
+else
+    echo "Space not pressed, aborting"
+    exit 1
+fi
+acme.sh --renew -d $HOSTNAME
+echo "Copying cert deploy to /usr/bin"
+cp ./cert-deploy.sh /usr/bin
+chmod 755 /usr/bin/cert-deploy.sh
