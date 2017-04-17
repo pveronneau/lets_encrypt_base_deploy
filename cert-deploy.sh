0bc9623e94535e2761821dcc772fb93e526a2995
pveronneau <patrick@decepticons.ca>
2017-04-17 14:03:58 -0600

    add additional cert types

--- cert-deploy.sh
+++ cert-deploy.sh
@@ -6,12 +6,24 @@
 ####
 HOSTNAME=`hostname`
 CERTLOCATION="/etc/$HOSTNAME.cert"
+PKCS12LOCATION="/etc/$HOSTNAME.pfx"
+SONARRLOCATION="/home/docker-config/sonarr"
 
 ####
-# Create Central File
+# Create combined pem file
 cat /root/.acme.sh/$HOSTNAME/$HOSTNAME.key > $CERTLOCATION && cat /root/.acme.sh/$HOSTNAME/$HOSTNAME.cer >> $CERTLOCATION
 chmod 600 $CERTLOCATION
 ####
+####
+# Create pkcs12 file
+####
+openssl pkcs12 -export -in /root/.acme.sh/$HOSTNAME/$HOSTNAME.cer -inkey /root/.acme.sh/$HOSTNAME/$HOSTNAME.key -out $PKCS12LOCATION -certfile /root/.acme.sh/$HOSTNAME/ca.cer -passout pass:
+chmod 600 $PKCS12LOCATION
+####
+# Create pvk file for Sonarr/Radarr
+####
+openssl rsa -in /root/.acme.sh/$HOSTNAME/$HOSTNAME.key -outform PVK -pvk-none -out $SONARRLOCATION/sonarr.pvk
+openssl x509 -inform PEM -in /root/.acme.sh/$HOSTNAME/$HOSTNAME.cer -outform DER -out $SONARRLOCATION/sonarr.cert
 
 ####
 # Deploy to servies
