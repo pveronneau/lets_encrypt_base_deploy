54ccdf48e95fea4ca165c169514cebc6699093de
Patrick Veronneau <patrick.veronneau@sjrb.ca>
2017-10-18 16:24:07 -0600

    updated renew command

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 HOSTNAME=`hostname`
 
 curl https://get.acme.sh | sh
-acme.sh --issue --dns -d $HOSTNAME
+acme.sh --issue --dns --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
 echo "Add the TXT entry above before proceding"
 read -n1 -r -p "Press space to continue... or any other key to abort" key
 
@@ -15,5 +15,5 @@ else
 fi
 acme.sh --renew -d $HOSTNAME
 echo "Copying cert deploy to /usr/bin"
-cp ./cert-deploy.sh /usr/bin
+cp ./cert-deploy.sh /usr/bin/cert-deploy.sh
 chmod 755 /usr/bin/cert-deploy.sh
