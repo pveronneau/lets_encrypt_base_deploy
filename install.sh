140d330a6854edb2a972021666faacb29655c348
Patrick Veronneau <patrick@decepticons.ca>
2018-02-22 14:57:00 -0700

    updated to use route 53

--- install.sh
+++ install.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+# AWS user must have the following policy
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "route53:ListHostedZones"
+#             ],
+#             "Resource": "*"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "route53:GetHostedZone",
+#                 "route53:ListResourceRecordSets",
+#                 "route53:ChangeResourceRecordSets"
+#             ],
+#             "Resource": "arn:aws:route53:::hostedzone/<PUT YOUR DOMAIN ID HERE>"
+#         }
+#     ]
+# }
+####
+##Set your AWS keys here
+export  AWS_ACCESS_KEY_ID=XXXXXXXXXX
+export  AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXX
+####
 
 HOSTNAME=`hostname`
 # Verify this is run by root
@@ -10,14 +37,5 @@ echo "Copying cert deploy to /usr/bin"
 cp ./cert-deploy.sh /usr/bin/cert-deploy.sh
 chmod 755 /usr/bin/cert-deploy.sh
 curl https://get.acme.sh | sh
-/root/.acme.sh/acme.sh --issue --dns --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
-echo "Add the TXT entry above before proceding"
-read -n1 -r -p "Press space to continue... or any other key to abort" key
-
-if [ "$key" = '' ]; then
-    echo "Renewing Certificate"
-else
-    echo "Space not pressed, aborting"
-    exit 1
-fi
+/root/.acme.sh/acme.sh --issue --dns dns_aws --deploy-hook /usr/bin/cert-deploy.sh --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
 /root/.acme.sh/acme.sh --renew -d $HOSTNAME
