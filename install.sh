fd696e127533c82ed67efba43325d71ef2f76ec3
Patrick Veronneau <patrick@decepticons.ca>
2018-02-21 20:54:26 -0700

    corrected path error in install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Copying cert deploy to /usr/bin"
 cp ./cert-deploy.sh /usr/bin/cert-deploy.sh
 chmod 755 /usr/bin/cert-deploy.sh
 curl https://get.acme.sh | sh
-acme.sh --issue --dns --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
+~/.acme.sh/acme.sh --issue --dns --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
 echo "Add the TXT entry above before proceding"
 read -n1 -r -p "Press space to continue... or any other key to abort" key
 
@@ -15,4 +15,4 @@ else
     echo "Space not pressed, aborting"
     exit 1
 fi
-acme.sh --renew -d $HOSTNAME
+~/.acme.sh/acme.sh --renew -d $HOSTNAME
