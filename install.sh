5a4bfdf2e57362e8fa4d04e90f6a894d7f6e2af4
Patrick Veronneau <patrick@decepticons.ca>
2018-02-21 21:06:28 -0700

    fix required user

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 HOSTNAME=`hostname`
+# Verify this is run by root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 echo "Copying cert deploy to /usr/bin"
 cp ./cert-deploy.sh /usr/bin/cert-deploy.sh
 chmod 755 /usr/bin/cert-deploy.sh
 curl https://get.acme.sh | sh
-~/.acme.sh/acme.sh --issue --dns --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
+/root/.acme.sh/acme.sh --issue --dns --renew-hook /usr/bin/cert-deploy.sh -d $HOSTNAME
 echo "Add the TXT entry above before proceding"
 read -n1 -r -p "Press space to continue... or any other key to abort" key
 
@@ -15,4 +20,4 @@ else
     echo "Space not pressed, aborting"
     exit 1
 fi
-~/.acme.sh/acme.sh --renew -d $HOSTNAME
+/root/.acme.sh/acme.sh --renew -d $HOSTNAME
